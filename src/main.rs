ef97865d685f6b2e49ebfe5a3de6297b688b16bc
Tomoya Zaitsu <zaitomo41@gmail.com>
2024-07-10 08:15:20 +0900

    actix_webを使ったテストコード

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Hoge {
+    hoge: String,
+    piyo: u8,
+}
+
+#[get("/json")]
+async fn json() -> impl Responder {
+    let hoge = Hoge {
+        hoge: "hoge piyo".to_string(),
+        piyo: 8,
+    };
+    HttpResponse::Ok().json(hoge)
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "こんちは。"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(json))
+        .bind("localhost:8080")?
+        .run()
+        .await
 }
