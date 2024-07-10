32d8ea9d78c44d75d3edd84ba39384d7d3a89307
Tomoya Zaitsu <zaitomo41@gmail.com>
2024-07-10 22:35:37 +0900

    テストコード変更

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,13 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use serde::Serialize;
+use actix_web::{web, App, HttpServer, Responder};
 
-#[derive(Serialize)]
-struct Hoge {
-    hoge: String,
-    piyo: u8,
-}
-
-#[get("/json")]
-async fn json() -> impl Responder {
-    let hoge = Hoge {
-        hoge: "hoge piyo".to_string(),
-        piyo: 8,
-    };
-    HttpResponse::Ok().json(hoge)
-}
-
-#[get("/")]
-async fn index() -> impl Responder {
-    "こんちは。"
+async fn greet() -> impl Responder {
+    "Hello, world!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(json))
-        .bind("localhost:8080")?
+    HttpServer::new(|| App::new().route("/", web::get().to(greet)))
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
