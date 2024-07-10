eb7d6d0e2af4d685d09f616f4a8a28937a99191f
Tomoya Zaitsu <zaitomo41@gmail.com>
2024-07-10 22:43:54 +0900

    portは環境設定からとってくる

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpServer, Responder};
+use std::env;
 
 async fn greet() -> impl Responder {
     "Hello, world!"
@@ -6,8 +7,11 @@ async fn greet() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
     HttpServer::new(|| App::new().route("/", web::get().to(greet)))
-        .bind("0.0.0.0:8080")?
+        .bind(addr)?
         .run()
         .await
 }
