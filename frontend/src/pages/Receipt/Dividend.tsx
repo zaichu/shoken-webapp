eb0164f86bd811bef9fbfdd00a894cfec70dcab5
zaichu <zaitomo41@gmail.com>
2025-05-04 19:12:05 +0900

    配当金の型変換処理を修正

--- frontend/src/pages/Receipt/Dividend.tsx
+++ frontend/src/pages/Receipt/Dividend.tsx
@@ -3,20 +3,20 @@ import React, { useMemo, useState } from 'react';
 import { formatCurrencyString, formatNumber } from '@/lib/utils/format';
 
 interface DividendData {
-    入金日: string;
+    入金日: Date;
     商品: string;
     口座: string;
     銘柄コード: string;
     銘柄: string;
-    '単価[円/現地通貨]': number;
-    '数量[株/口]': number;
-    '配当・分配金合計（税引前）[円/現地通貨]': string;
-    '税額合計[円/現地通貨]': string;
-    '受取金額[円/現地通貨]': string;
+    単価: number;
+    数量: number;
+    配当・分配金合計: number;
+    税額合計: number;
+    受取金額: number;
 }
 
 interface DividendProps {
-    csvData: DividendData[];
+    csvData: any[];
 }
 
 interface Calculations {
@@ -54,42 +54,51 @@ const Header: React.FC<{ calculations: Calculations }> = ({ calculations }) => {
 export const Dividend: React.FC<DividendProps> = ({ csvData }) => {
     const [searchQuery, setSearchQuery] = useState('');
     const onSearch = (query: string) => setSearchQuery(query);
-
-    csvData = useMemo(() => {
-        return [...csvData].sort((a, b) => {
-            const dateA = new Date(a['入金日'].replace(/(\d+)\/(\d+)\/(\d+)/, '$1-$2-$3'));
-            const dateB = new Date(b['入金日'].replace(/(\d+)\/(\d+)\/(\d+)/, '$1-$2-$3'));
-            return dateA.getTime() - dateB.getTime();
-        });
+    var dividendData: DividendData[] = useMemo(() => {
+        return csvData.map((item) => ({
+            ...item,
+            入金日: new Date(item['入金日']),
+            単価: Number(item['単価[円/現地通貨]'].replace(/,/g, '')),
+            数量: Number(item['数量[株/口]']),
+            配当・分配金合計: Number(item['配当・分配金合計（税引前）[円/現地通貨]'].replace(/,/g, '') || 0),
+            税額合計: Number(item['税額合計[円/現地通貨]'].replace(/,/g, '') || 0),
+            受取金額: Number(item['受取金額[円/現地通貨]'].replace(/,/g, '') || 0),
+        }));
     }, [csvData]);
 
+    dividendData = useMemo(() => {
+        return [...dividendData].sort((a, b) => {
+            return a.入金日.getTime() - b.入金日.getTime();
+        });
+    }, [dividendData]);
+
     const calculations = useMemo(() => {
-        return csvData.reduce((acc, item) => {
+        return dividendData.reduce((acc, item) => {
             return {
-                totalDividends: acc.totalDividends + Number(item['配当・分配金合計（税引前）[円/現地通貨]'].replace(/,/g, '') || 0),
-                totalTaxes: acc.totalTaxes + Number(item['税額合計[円/現地通貨]'].replace(/,/g, '') || 0),
-                totalNetAmount: acc.totalNetAmount + Number(item['受取金額[円/現地通貨]'].replace(/,/g, '') || 0)
+                totalDividends: acc.totalDividends + item.配当・分配金合計,
+                totalTaxes: acc.totalTaxes + item.税額合計,
+                totalNetAmount: acc.totalNetAmount + item.受取金額,
             };
         }, {
             totalDividends: 0,
             totalTaxes: 0,
             totalNetAmount: 0
         });
-    }, [csvData]);
+    }, [dividendData]);
 
     const searchOptions =
-        csvData.map((item) => ({
-            value: item['銘柄コード'] ? item['銘柄コード'] : item['銘柄'],
-            label: item['銘柄コード'] ? item['銘柄コード'] + ':' + item['銘柄'] : item['銘柄']
+        dividendData.map((item) => ({
+            value: item.銘柄コード ? item.銘柄コード : item.銘柄,
+            label: item.銘柄コード ? item.銘柄コード + ':' + item.銘柄 : item.銘柄,
         })).filter((item, index, self) =>
             index === self.findIndex((t) => (
                 t.value === item.value
             ))
         ).sort((a, b) => a.value.localeCompare(b.value));
 
-    const filteredData = csvData.filter((item) => {
+    const filteredData = dividendData.filter((item) => {
         const searchValue = searchQuery.toLowerCase();
-        return item['銘柄コード'].toLowerCase().includes(searchValue) || item['銘柄'].toLowerCase().includes(searchValue);
+        return item.銘柄コード.toLowerCase().includes(searchValue) || item.銘柄.toLowerCase().includes(searchValue);
     });
 
     const headerName = ['入金日', '商品', '口座', '銘柄コード', '銘柄名', '単価', '数量[株]', '配当・分配金', '税額', '受取金額', '配当・分配金合計', '税額合計', '受取金額合計'];
@@ -106,20 +115,20 @@ export const Dividend: React.FC<DividendProps> = ({ csvData }) => {
                 <TableBody>
                     {filteredData.map((item, index) => (
                         <TableRow key={index}>
-                            <TableCell>{item['入金日']}</TableCell>
-                            <TableCell>{item['商品']}</TableCell>
-                            <TableCell style={{ width: '100px' }}>{item['口座']}</TableCell>
-                            <TableCell>{item['銘柄コード']}</TableCell>
-                            <TableCell style={{ width: '250px' }}>{item['銘柄']}</TableCell>
-                            <TableCell style={{ width: '80px', textAlign: 'right' }}>{formatCurrencyString(item['単価[円/現地通貨]'])}</TableCell>
-                            <TableCell style={{ width: '100px', textAlign: 'right' }}>{formatNumber(item['数量[株/口]'])}</TableCell>
-                            <TableCell style={{ width: '150px', textAlign: 'right' }}>{formatCurrencyString(item['配当・分配金合計（税引前）[円/現地通貨]'])}</TableCell>
-                            <TableCell style={{ width: '100px', textAlign: 'right' }}>{formatCurrencyString(item['税額合計[円/現地通貨]'])}</TableCell>
-                            <TableCell style={{ width: '100px', textAlign: 'right' }}>{formatCurrencyString(item['受取金額[円/現地通貨]'])}</TableCell>
+                            <TableCell>{item.入金日.toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit', })}</TableCell>
+                            <TableCell>{item.商品}</TableCell>
+                            <TableCell style={{ width: '100px' }}>{item.口座}</TableCell>
+                            <TableCell>{item.銘柄コード}</TableCell>
+                            <TableCell style={{ width: '250px' }}>{item.銘柄}</TableCell>
+                            <TableCell style={{ width: '80px', textAlign: 'right' }}>{formatCurrencyString(item.単価)}</TableCell>
+                            <TableCell style={{ width: '100px', textAlign: 'right' }}>{formatNumber(item.数量)}</TableCell>
+                            <TableCell style={{ width: '150px', textAlign: 'right' }}>{formatCurrencyString(item.配当・分配金合計)}</TableCell>
+                            <TableCell style={{ width: '100px', textAlign: 'right' }}>{formatCurrencyString(item.税額合計)}</TableCell>
+                            <TableCell style={{ width: '100px', textAlign: 'right' }}>{formatCurrencyString(item.受取金額)}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-        </ReceiptTemplate>
+        </ReceiptTemplate >
     )
 };
