139a19deb2490d747e3396030e364f3b6a50d3ec
Tomoya Zaitsu <zaitomo41@gmail.com>
2026-04-07 08:58:49 +0900

    chore: frontend の ESLint 10 対応 (#330)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 import reactCompilerPlugin from 'eslint-plugin-react-compiler';
@@ -24,13 +23,11 @@ export default tseslint.config(
       },
     },
     plugins: {
-      'react': reactPlugin,
       'react-hooks': reactHooksPlugin,
       'react-refresh': reactRefreshPlugin,
       'react-compiler': reactCompilerPlugin,
     },
     rules: {
-      ...reactPlugin.configs.recommended.rules,
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': [
@@ -38,13 +35,6 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'react-compiler/react-compiler': 'error',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
     },
   },
 );
